c7cb36e6872895f840602db07138540db1409331
Douglas Oliveira <douglas.olliveira1997@gmail.com>
2023-10-28 23:29:09 -0300

    feat:Atualizando projetos

--- src/app/components/projetos/projetos.component.ts
+++ src/app/components/projetos/projetos.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-projetos',
@@ -6,6 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projetos.component.css']
 })
 export class ProjetosComponent implements OnInit {
+  @Input()
+  html:string = "../../../assets/html-5.png";
+  @Input()
+  css:string= "../../../assets/css-3.png";
+  @Input()
+ bootstrap:string = '../../../assets/bootstrap.png';
+ @Input()
+ js:string = "../../../assets/js.png";
+ @Input()
+ ty:string = '../../../assets/typescript.png';
+ @Input()
+ sass:string = '../../../assets/sass.png';
+ @Input()
+ angular:string ='../../../assets/angular.png';
+ @Input()
+ github:string = '../../../assets/placa-do-github.png';
 
   constructor() { }
 
