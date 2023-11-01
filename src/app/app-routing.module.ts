3e93509491f5fe7a010a17b310374fb6bc5bbc08
Douglas Oliveira <douglas.olliveira1997@gmail.com>
2023-11-01 16:26:04 -0300

    feat: modificando responsividade

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { SobreComponent } from './page/sobre/sobre.component';
 import { HomeComponent } from './home/home/home.component';
 
 import { SobreMimComponent } from './components/sobre-mim/sobre-mim.component';
+import { ProjetosComponent } from './components/projetos/projetos.component';
 
 const routes: Routes = [{
   path: '', component:HomeComponent
@@ -13,7 +14,8 @@ const routes: Routes = [{
 {
   path: 'sobre', component:SobreComponent,pathMatch:'full'
 },
-{path:'sobre-mim-aba',component:SobreMimComponent,pathMatch:'full'}
+{path:'sobre-mim-aba',component:SobreMimComponent,pathMatch:'full'},
+{path:'projetos', component:ProjetosComponent,pathMatch:'full'}
 ];
 
 @NgModule({
--- src/app/components/projetos/projetos.component.ts
+++ src/app/components/projetos/projetos.component.ts
@@ -7,6 +7,7 @@ import { Component, Input, OnInit } from '@angular/core';
 
   styleUrls: ['./projetos.component.css'],
   template:`
+  <h2>Projetos: </h2>
   <div class="card-group">
     <article class="card" *ngFor="let projeto of projetos">
       <h3>{{ projeto.titles }}</h3>
@@ -38,27 +39,83 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ProjetosComponent implements OnInit {
   projetos = [
+    //projeto 1
     {
+
       titles: 'progBOOKS',
       img: '../../../assets/progBOOKS.png',
-      linkDeploy: 'https://seu-link-deploy-1.com',
-      linkGithub: 'https://seu-link-repositorio-1.com',
+      linkDeploy: 'https://prog-book.vercel.app/',
+      linkGithub: 'https://github.com/douglas1997silva/ProgBook',
       tecnologias: [
         { nome: 'HTML', imagem: '../../../assets/html-5.png' },
         { nome: 'CSS', imagem: '../../../assets/css-3.png' },
       ],
-      textprojetos: 'Descrição do Projeto 1',
+      textprojetos: 'Criei um site com HTML, CSS e JavaScript para uma Biblioteca de Livros Dev. Apresenta um menu "hamburguer" e um slide para exibir informações sobre os livros em destaque. Não utiliza banco de dados. Destaca minhas habilidades em front-end web development.',
     },
+    //projeto 2
     {
-      titles: 'Projeto 2',
-      img: 'caminho-para-imagem-projeto2.jpg',
-      linkDeploy: 'https://seu-link-deploy-2.com',
-      linkGithub: 'https://seu-link-repositorio-2.com',
+      titles: 'Meteora',
+      img: '../../../assets/meteoro.png',
+      linkDeploy: 'https://brootrap.vercel.app/',
+      linkGithub: 'https://github.com/douglas1997silva/Meteora',
       tecnologias: [
-        { nome: 'Angular', imagem: 'caminho-para-imagem-angular.jpg' },
-        { nome: 'JavaScript', imagem: 'caminho-para-imagem-js.jpg' },
+        { nome: 'HTML', imagem: '../../../assets/html-5.png' },
+        { nome: 'CSS', imagem: '../../../assets/css-3.png' },
+        {nome: 'bootstrap', imagem: '../../../assets/bootstrap.png'}
       ],
-      textprojetos: 'Descrição do Projeto 2',
+      textprojetos: 'Desenvolvi uma loja online com HTML, CSS e Bootstrap. Este site oferece uma plataforma de compras responsiva para produtos diversos. Os clientes podem explorar itens, visualizar detalhes e concluir compras de maneira eficiente. Destaca minhas habilidades em design de lojas virtuais.',
+    },
+    //projeto 3
+    {
+      titles: ' WebTempo',
+      img: '../../../assets/webtempo.png',
+      linkDeploy: 'https://webtempo.vercel.app/',
+      linkGithub: 'https://github.com/douglas1997silva/webtempo',
+      tecnologias: [
+        { nome: 'HTML', imagem: '../../../assets/html-5.png' },
+        { nome: 'CSS', imagem: '../../../assets/css-3.png' },
+        {nome: 'javaScript', imagem: '../../../assets/js.png'}
+      ],
+      textprojetos: 'Criei um aplicativo de previsão do clima que fornece informações meteorológicas personalizadas com base na localização do usuário. Utilizando HTML, CSS e JavaScript, o aplicativo oferece dados atualizados sobre o clima, como temperatura, condições climáticas e previsões.',
+    },
+    //projeto 4
+    {
+      titles: ' Mochila de Viajem',
+      img: '../../../assets/mochiladeviajem.png',
+      linkDeploy: 'https://mochila-de-viagem-lemon.vercel.app/',
+      linkGithub: 'https://github.com/douglas1997silva/Mochila-de-viagem',
+      tecnologias: [
+        { nome: 'HTML', imagem: '../../../assets/html-5.png' },
+        { nome: 'CSS', imagem: '../../../assets/css-3.png' },
+        {nome: 'javaScript', imagem: '../../../assets/js.png'}
+      ],
+      textprojetos: 'Criei uma "Mochila de Viagem" com HTML, CSS e JavaScript, utilizando o localStorage. Agora é possível adicionar e manter itens na mochila, mesmo após fechar o navegador, garantindo que nunca sejam perdidos. Destaco minhas habilidades em desenvolver soluções práticas para viajantes.',
+    },
+    //projeto 4
+    {
+      titles: 'Formulario',
+      img: '../../../assets/formulario.png',
+      linkDeploy: 'https://validando-formulario-nu.vercel.app/pages/abrir-conta-form.html',
+      linkGithub: 'https://github.com/douglas1997silva/Validando-formulario',
+      tecnologias: [
+        { nome: 'HTML', imagem: '../../../assets/html-5.png' },
+        { nome: 'CSS', imagem: '../../../assets/css-3.png' },
+        {nome: 'javaScript', imagem: '../../../assets/js.png'}
+      ],
+      textprojetos: 'Desenvolvi um formulário para um site de banco utilizando HTML, CSS e JavaScript. Implementei recursos de detecção de erros, ajudando os usuários a evitar equívocos ao preencher informações sensíveis. Esse projeto destaca minha habilidade em melhorar a usabilidade e segurança em formulários online.',
+    },
+    //projeto 5
+    {
+      titles: 'Formulario',
+      img: '../../../assets/formulario.png',
+      linkDeploy: 'https://validando-formulario-nu.vercel.app/pages/abrir-conta-form.html',
+      linkGithub: 'https://github.com/douglas1997silva/Validando-formulario',
+      tecnologias: [
+        { nome: 'HTML', imagem: '../../../assets/html-5.png' },
+        { nome: 'CSS', imagem: '../../../assets/css-3.png' },
+        {nome: 'javaScript', imagem: '../../../assets/js.png'}
+      ],
+      textprojetos: 'Desenvolvi um formulário para um site de banco utilizando HTML, CSS e JavaScript. Implementei recursos de detecção de erros, ajudando os usuários a evitar equívocos ao preencher informações sensíveis. Esse projeto destaca minha habilidade em melhorar a usabilidade e segurança em formulários online.',
     },
     // Outros projetos
   ];
