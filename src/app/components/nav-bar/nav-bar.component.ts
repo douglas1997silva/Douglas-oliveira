6606f8653fa84d7c6ef2f473d85bc56b42bf3c2d
Douglas Oliveira <douglas.olliveira1997@gmail.com>
2023-11-03 18:33:44 -0300

    feat: Adicionando funcionamneto

--- src/app/components/nav-bar/nav-bar.component.ts
+++ src/app/components/nav-bar/nav-bar.component.ts
@@ -9,15 +9,19 @@ import { Component, OnInit } from '@angular/core';
 export class NavBarComponent implements OnInit {
    isMenuOpen = false ;
    navbardark = false;
-
+   extit:boolean = false ;
 
   constructor() { }
   toggleMenu():void{
    this.isMenuOpen = !this.isMenuOpen;
 
+
   }
 
+  sair(){
+   this.extit = !this.extit;
 
+  }
   changeThemeColor(){
     document.body.classList.toggle('dark-theme');
     this.navbardark = ! this.navbardark;
